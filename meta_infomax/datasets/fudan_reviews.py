e9f801837cff7657cf2736c18b583e002fc49bf4
Omar Elbaghdadi <omarelb@gmail.com>
2020-04-28 18:45:40 +0200

    move functions to utils and remove torchtext

--- meta_infomax/datasets/fudan_reviews.py
+++ meta_infomax/datasets/fudan_reviews.py
@@ -1,7 +1,5 @@
 from pathlib import Path
 import pandas as pd
-from torchtext.data import Field, TabularDataset, BucketIterator
-from torchtext.vocab import GloVe
 
 from typing import Iterator, List, Dict, Tuple, Union
 import torch
@@ -26,48 +24,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 DATA_DIR = Path("../data/mtl-dataset")
-GLOVE_DIR = Path("../data/glove")
 DATASETS = ['apparel', 'baby', 'books', 'camera_photo',  'electronics', 
       'health_personal_care', 'imdb', 'kitchen_housewares', 'magazines', 
       'music', 'software', 'sports_outdoors', 'toys_games', 'video']
-
-def prepare_data(dataset_list: list=DATASETS, data_dir: Path=DATA_DIR,
-                 devset_fraction: float=0.2) -> None:
-    col_names=['label', 'text']
-    train_dfs = []
-    test_dfs = []
-    for idx, dataset in enumerate(dataset_list):
-        train_file = dataset+".task.train"
-        test_file = dataset+".task.test"
-        train_df = pd.read_csv(data_dir / train_file, sep="\t", header=None, names=col_names)
-        test_df = pd.read_csv(data_dir / test_file, sep="\t", header=None, names=col_names)
-        train_df['task_id'] = idx
-        train_df['task_name'] = dataset
-        test_df['task_id'] = idx
-        test_df['task_name'] = dataset
-        train_dfs.append(train_df)
-        test_dfs.append(test_df)
-    # concatenate then shuffle in place
-    df = pd.concat(train_dfs)
-    train_dfs = df.sample(frac=1-devset_fraction).reset_index(drop=True)
-    dev_dfs = df.sample(frac=devset_fraction).reset_index(drop=True)
-    test_dfs = pd.concat(test_dfs).sample(frac=1).reset_index(drop=True)
-    train_dfs.to_csv(DATA_DIR / "train.csv", index=False)
-    dev_dfs.to_csv(DATA_DIR / "dev.csv", index=False)
-    test_dfs.to_csv(DATA_DIR / "test.csv", index=False)
-
-def get_data(train_file: str="train.csv", dev_file: str="dev.csv", test_file: str="test.csv",
-             tokenizer: any=lambda x: x.split(), include_lengths: bool=True):
-    text_field = Field(sequential=True, tokenize=tokenizer, lower=True, batch_first=True, include_lengths=True)
-    label_field = Field(sequential=False, use_vocab=False)
-    task_field = Field(sequential=False, use_vocab=False)
-    data_fields = [("label", label_field), ("text", text_field), ("task", task_field), ("task_name", None)]
-    train_set, dev_set, test_set = TabularDataset.splits(path=DATA_DIR, root=DATA_DIR,
-                                                        train=train_file, validation=dev_file, test=test_file,
-                                                        fields=data_fields, skip_header=True, format="csv")
-    text_field.build_vocab(train_set, vectors=GloVe(cache=GLOVE_DIR))
-    return train_set, dev_set, test_set, text_field.vocab
-    reader = FudanReviewDatasetReader()
 
 
 def get_fudan_datasets(datasets: List[str]=DATASETS, data_dir: Union[str, Path]=DATA_DIR,
@@ -150,42 +109,4 @@ class FudanReviewDatasetReader(DatasetReader):
                 print(e, f'skipping row {i}')
 
 
-def get_glove_embeddings(vocab: Vocabulary, glove_dir: Union[Path, str]=GLOVE_DIR,
-                        glove_file: Union[Path, str]='glove.840B.300d.txt'):
-    """Load Glove embeddings from file and generate embeddings."""
-    glove_dir = Path(glove_dir)
-    glove_params = Params({
-        'pretrained_file': (glove_dir / glove_file).as_posix(),
-        'embedding_dim': 300,
-        'trainable': False
-    })
-    return Embedding.from_params(vocab, glove_params)
 
-
-def sample_domains(data: Dict[str, Dict[str, List[Instance]]], n_samples: int=5, strategy: str='uniform') -> np.ndarray:
-    """Sample domains from data according to strategy
-    
-    Parameters
-    ----------
-    data: contains all data splits for domains
-    n_samples: number of samples to draw
-    strategy: in {'uniform', 'domain_size'}
-        specifies sampling strategy:
-        'uniform': sample uniformly
-        'domain_size': sample more from domain if size of domain is larger
-        
-    Returns
-    -------
-    array of domain names
-    """
-    assert strategy in ('uniform', 'domain_size'), 'specify correct strategy'
-    domains = np.array([d for d in data.keys()])
-    n_domains = len(domains)
-    if strategy == 'uniform':
-        weights = [1] * n_domains
-    elif strategy == 'domain_size':
-        weights = [len(data[domain]['train']) for domain in domains]
-
-    sampler = torch.utils.data.WeightedRandomSampler([1 / n_domains] * n_domains, num_samples=n_samples,
-                                                     replacement=False)
-    return domains[list(sampler)]

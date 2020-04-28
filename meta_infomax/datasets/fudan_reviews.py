3519cf6df3b7bc3220a7f24f7fa68111f80145c8
Omar Elbaghdadi <omarelb@gmail.com>
2020-04-28 12:39:10 +0200

    add review dataset reader class

--- meta_infomax/datasets/fudan_reviews.py
+++ meta_infomax/datasets/fudan_reviews.py
@@ -3,6 +3,32 @@ import pandas as pd
 from torchtext.data import Field, TabularDataset, BucketIterator
 from torchtext.vocab import GloVe
 
+from typing import Iterator, List, Dict
+import torch
+import numpy as np
+
+from allennlp.common.params import Params
+from allennlp.data import Instance
+from allennlp.data.fields import TextField, LabelField
+from allennlp.data.dataset_readers import DatasetReader
+from allennlp.common.file_utils import cached_path
+from allennlp.data.token_indexers import TokenIndexer, SingleIdTokenIndexer
+from allennlp.data.tokenizers import Token
+from allennlp.data.vocabulary import Vocabulary
+from allennlp.models import Model
+from allennlp.modules.text_field_embedders import TextFieldEmbedder, BasicTextFieldEmbedder
+from allennlp.modules.token_embedders import Embedding
+from allennlp.data.iterators import BucketIterator
+from allennlp.common.testing import AllenNlpTestCase
+from allennlp.common.util import ensure_list
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+
+torch.manual_seed(1)
+%load_ext autoreload
+%autoreload 2
+
 DATA_DIR = Path("../data/mtl-dataset")
 GLOVE_DIR = Path("../data")
 DATASETS = ['apparel', 'baby', 'books', 'camera_photo',  'electronics', 
@@ -45,3 +71,34 @@ def get_data(train_file: str="train.csv", dev_file: str="dev.csv", test_file: st
                                                         fields=data_fields, skip_header=True, format="csv")
     text_field.build_vocab(train_set, vectors=GloVe(cache=GLOVE_DIR))
     return train_set, dev_set, test_set, text_field.vocab
+
+@DatasetReader.register("fudan_review") # allows us to specify reader using JSON
+class FudanReviewDatasetReader(DatasetReader):
+    """
+    DatasetReader for Fudan Review data, one sentence per line, like
+
+        <sentiment_label in (0,1)><tab><sentence>
+        
+    No need to tokenize sentence, since that's already been done.
+    """
+    
+    def __init__(self, token_indexers: Dict[str, TokenIndexer] = None) -> None:
+        super().__init__(lazy=False)
+        self.token_indexers = token_indexers or {'tokens': SingleIdTokenIndexer(lowercase_tokens=False)}
+    
+    def text_to_instance(self, tokens: List[Token], label: int = None) -> Instance:
+        sentence_field = TextField(tokens, self.token_indexers)
+        fields = {'sentence': sentence_field}
+
+        if label is not None:
+            # our labels are already 0-indexed ints -> skip indexing
+            label_field = LabelField(label=label, skip_indexing=True)
+            fields['label'] = label_field
+
+        return Instance(fields)
+    
+    def _read(self, file_path: str) -> Iterator[Instance]:
+        with open(file_path) as f:
+            for line in f:
+                label, sentence = line.strip(' ').split('\t')
+                yield self.text_to_instance([Token(word) for word in sentence.split()], int(label))

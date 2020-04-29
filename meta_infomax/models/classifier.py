5d6e1ea33cb0eec1537cef1ce1f74b78c66a6638
Omar Elbaghdadi <omarelb@gmail.com>
2020-04-29 10:34:10 +0200

    add initial sentiment classifier

--- meta_infomax/models/classifier.py
+++ meta_infomax/models/classifier.py
@@ -91,3 +91,69 @@ class FeedForward(nn.Module):
             output = dropout(activation(layer(output)))
         output = self._linear_layers[-1](output)
         return output
+
+class SentimentClassifier(nn.Module):
+    def __init__(self,
+                 text_encoder,
+                 classifier_feedforward,
+                ):
+        """
+        Parameters
+        ----------
+        """
+        super().__init__()
+
+        self.text_encoder = text_encoder
+        self.num_classes = 2
+        self.classifier_feedforward = classifier_feedforward
+        self.criterion = torch.nn.CrossEntropyLoss()
+
+    def forward(self,
+                text: Dict[str, torch.LongTensor],
+                label: torch.LongTensor = None) -> Dict[str, torch.Tensor]:
+        """
+        Parameters
+        ----------
+        text : Dict[str, Variable], required
+            The output of ``TextField.as_array()``.
+        label : Variable, optional (default = None)
+            A variable representing the label for each instance in the batch.
+            
+        Returns
+        -------
+        An output dictionary consisting of:
+        class_probabilities : torch.FloatTensor
+            A tensor of shape ``(batch_size, num_classes)`` representing a distribution over the
+            label classes for each instance.
+        loss : torch.FloatTensor, optional
+            A scalar loss to be optimised.
+        """
+        encoded_text = self.text_encoder(text)[0][:,0] # todo: make abstraction model that
+
+        logits = self.classifier_feedforward(encoded_text)
+        output_dict = {'logits': logits}
+
+        if label is not None:
+            loss = self.criterion(logits, label)
+#             acc = accuracy(logits, label)
+            output_dict["loss"] = loss
+#             output_dict["acc"] = acc
+
+        return output_dict
+    
+    def decode(self, output_dict: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        """
+        Does a simple argmax over the class probabilities, converts indices to string labels, and
+        adds a ``"label"`` key to the dictionary with the result.
+        """
+        predictions = output_dict['class_probabilities'].cpu().data.numpy()
+        argmax_indices = np.argmax(predictions, axis=-1)
+#         labels = [self.vocab.get_token_from_index(x, namespace="labels")
+#                   for x in argmax_indices]
+        labels = -1 # TODO
+        output_dict['label'] = labels
+        return output_dict
+    
+    
+    def get_metrics(self, reset: bool = False) -> Dict[str, float]:
+        return {metric_name: metric.get_metric(reset) for metric_name, metric in self.metrics.items()}

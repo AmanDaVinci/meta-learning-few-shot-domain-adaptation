581591c5b36bde326d04ef2072422f15bafac320
Ivan Bardarov <iebardarov@gmail.com>
2020-04-30 11:23:19 +0200

    Format fudan_reviews.py and fix imports

--- meta_infomax/datasets/fudan_reviews.py
+++ meta_infomax/datasets/fudan_reviews.py
@@ -1,25 +1,18 @@
 from pathlib import Path
-import pandas as pd
-
-from typing import Iterator, List, Dict, Tuple, Union
-import torch
-import numpy as np
-
-import torchtext
-from torchtext.data import Field, LabelField, TabularDataset, Dataset
-
 from sklearn.model_selection import train_test_split
+from torchtext.data import Field, LabelField, TabularDataset, Dataset
+from typing import List, Dict, Union
 
 DATA_DIR = Path("../data/mtl-dataset")
-DATASETS = ['apparel', 'baby', 'books', 'camera_photo',  'electronics', 
-      'health_personal_care', 'imdb', 'kitchen_housewares', 'magazines', 
-      'music', 'software', 'sports_outdoors', 'toys_games', 'video']
+DATASETS = ['apparel', 'baby', 'books', 'camera_photo', 'electronics',
+            'health_personal_care', 'imdb', 'kitchen_housewares', 'magazines',
+            'music', 'software', 'sports_outdoors', 'toys_games', 'video']
 MODEL_NAME = 'bert-base-uncased'
 
 
-def get_fudan_datasets(tokenizer, datasets: List[str]=DATASETS, data_dir: Union[str, Path]=DATA_DIR,
-                       validation_size: float=0.2, min_count: int=0,
-                       random_state: int=11) -> Dict[str, Dict[str, List[Dataset]]]:
+def get_fudan_datasets(tokenizer, datasets: List[str] = DATASETS, data_dir: Union[str, Path] = DATA_DIR,
+                       validation_size: float = 0.2, min_count: int = 0,
+                       random_state: int = 11) -> Dict[str, Dict[str, List[Dataset]]]:
     """
     Get all fudan datasets specified in a dictionary. Also return vocabulary.
     Gets all train, val, and test tests, with corresponding keys in the returned dict for each dataset.
@@ -46,17 +39,17 @@ def get_fudan_datasets(tokenizer, datasets: List[str]=DATASETS, data_dir: Union[
     result = {}
 
     text_field = Field(sequential=True, use_vocab=False, tokenize=tokenizer.encode, batch_first=True, include_lengths=True,
-                        pad_token=tokenizer.pad_token_id, unk_token=tokenizer.unk_token_id)
+                       pad_token=tokenizer.pad_token_id, unk_token=tokenizer.unk_token_id)
     label_field = LabelField(use_vocab=False)
     data_fields = [("label", label_field), ("text", text_field)]
     for idx, dataset in enumerate(datasets):
         print(f'processing dataset: {dataset}')
-        train_file = dataset+".task.train"
-        test_file = dataset+".task.test"
+        train_file = dataset + ".task.train"
+        test_file = dataset + ".task.test"
         train_val_set, test_set, = TabularDataset.splits(path=data_dir,
-                                                            train=train_file,
-                                                            test=test_file,
-                                                            fields=data_fields, skip_header=True, format="tsv")
+                                                         train=train_file,
+                                                         test=test_file,
+                                                         fields=data_fields, skip_header=True, format="tsv")
         # tabulardataset reads it in as a string
         for example in train_val_set.examples:
             example.label = int(example.label)
@@ -68,4 +61,3 @@ def get_fudan_datasets(tokenizer, datasets: List[str]=DATASETS, data_dir: Union[
         result[dataset] = {'train': train_set, 'val': val_set, 'test': test_set}
 
     return result
-    

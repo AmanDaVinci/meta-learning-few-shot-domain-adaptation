c4e6d931f120372abc510e6821b7f223cf796dcc
Ivan Bardarov <iebardarov@gmail.com>
2020-05-01 19:29:08 +0200

    Download fudan dataset if missing

--- meta_infomax/datasets/fudan_reviews.py
+++ meta_infomax/datasets/fudan_reviews.py
@@ -2,6 +2,11 @@ from pathlib import Path
 from sklearn.model_selection import train_test_split
 from torchtext.data import Field, LabelField, TabularDataset, Dataset
 from typing import List, Dict, Union
+import shutil
+import urllib.request
+import tarfile
+import os
+from pathlib import Path
 
 DATASETS = ['apparel', 'baby', 'books', 'camera_photo', 'electronics',
             'health_personal_care', 'imdb', 'kitchen_housewares', 'magazines',
@@ -34,6 +39,11 @@ def get_fudan_datasets(tokenizer, data_dir: Union[str, Path], datasets: List[str
     dict mapping dataset to dict containing train, val, and test sets.
     """
     data_dir = Path(data_dir)
+
+    if not os.path.exists(data_dir):
+        data_dir.parent.mkdir(parents=True, exist_ok=True)
+        download_and_extract_fudan(data_dir.parent)
+
     result = {}
 
     trunkated_encode = lambda x: tokenizer.encode(x, max_length=512)
@@ -60,3 +70,22 @@ def get_fudan_datasets(tokenizer, data_dir: Union[str, Path], datasets: List[str
         result[dataset] = {'train': train_set, 'val': val_set, 'test': test_set}
 
     return result
+
+
+def download_and_extract_fudan(data_dir):
+    """
+    Downloads and extracts fudan data
+
+    Parameters
+    ----------
+    data_dir:
+        Directory of fudan datasets.
+    """
+    print("Downloading data...")
+    data_url = "https://github.com/FrankWork/fudan_mtl_reviews/raw/master/data/fudan-mtl-dataset.tar.gz"
+    tar_filename = data_dir / "fudan-mtl-dataset.tar.gz"
+    with urllib.request.urlopen(data_url) as response, open(tar_filename, 'wb') as out_file:
+        shutil.copyfileobj(response, out_file)
+    with tarfile.open(tar_filename, 'r:gz') as tar_file:
+        tar_file.extractall(data_dir)
+    os.remove(tar_filename)

68966c3d1b2834ef3e359ab9d05d9be35fb0d822
BalintHompot <hompot.balint@gmail.com>
2020-05-04 18:55:21 +0200

    maml config fixed

--- configs/multitask_config.py
+++ configs/multitask_config.py
@@ -1,15 +1,12 @@
+
+
 import logging
 from .base_config import base_config, get_config
 
 new_config = {
-    'trainer' : 'maml',
-    'k_shot_num' : 5,
-    'n_domains' : 5,
-    'fast_weight_lr' : 1e-3,
-    'meta_lr' : 1e-3, 
-    'collapse_domain' : False,
     'exp_name': 'multitask_test',
-    'episodes': 100,
+    'epochs': 1,
+    'batch_size': 4,
 }
 
 config = get_config(base_config, new_config)

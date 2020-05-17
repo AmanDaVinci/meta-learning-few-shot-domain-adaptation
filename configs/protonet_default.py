35aa697834456744c2190fa7e900d384a9f9ed2b
amandavinci <amanthevinci@gmail.com>
2020-05-17 18:33:46 +0200

    add freezing and total_seen_examples in config

--- configs/protonet_default.py
+++ configs/protonet_default.py
@@ -7,6 +7,8 @@ new_config = {
     'val_episodes': 10,
     'n_support': 10,
     'n_query': 20,
+    'freeze_until_layer': 10,
+    'total_seen_examples': 14000,
 }
 
 config = get_config(base_config, new_config)

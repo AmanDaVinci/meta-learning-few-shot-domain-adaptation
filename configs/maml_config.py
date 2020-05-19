3f0e771993974ae39bec0048031b375f69a12652
Bálint   Hompot <lgpu0015@test3.irc.sara.nl.irc.sara.nl>
2020-05-19 12:27:13 +0200

    fomaml runs

--- configs/maml_config.py
+++ configs/maml_config.py
@@ -3,7 +3,7 @@ from .base_config import base_config, get_config
 
 new_config = {
     'trainer' : 'maml',
-    'k_shot_num' : 5,
+    'k_shot_num' : 4,
     'num_batches_for_query' : 4,        ## concatenate that many bacthes of size k-shot for validation/test
     'n_domains' : 5,
     'fast_weight_lr' : 5e-5,
@@ -12,11 +12,12 @@ new_config = {
     'exp_name': 'maml_train',
     'episodes': 500,
     'test_episodes':40,
-    'val_episodes' : 5,
+    'val_episodes' : 20,
     'test_episodes' : 5,
     'domain_sampling_strategy' : "uniform",
     'inner_gd_steps': 3,
-    'num_examples' : 'all',
+    'unfreeze_layers': (10, 11),
+    'num_examples' : 3500,
 }
 
 config = get_config(base_config, new_config)

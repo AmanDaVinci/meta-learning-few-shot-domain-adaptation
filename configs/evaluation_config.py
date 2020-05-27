0a81a4a81682575868774bea11968dc762cc741c
Omar Elbaghdadi <omarelb@gmail.com>
2020-05-27 17:41:33 +0200

    change configs

--- configs/evaluation_config.py
+++ configs/evaluation_config.py
@@ -3,15 +3,16 @@ from .base_config import base_config, get_config
 
 new_config = {
     'trainer': 'evaluation',
-    'exp_name': 'multitask_11-12',
+    'exp_name': 'multitask_10-11-3500-sports-dvd',
     'weight_decay': 0,
     'lr': 1e-5,
-    'epochs': 3,
+    'epochs': 1,
     'device': 'cuda',
     'batch_size': 32,
-    'n_evaluations': 3, # run the experiment with this many different training batches
+    'n_evaluations': 1, # run the experiment with this many different training batches
     'warmup_steps': 0, # number of steps to warm up for learning rate scheduler
-    'k_shot': 4, # train on this many samples from test domain
+    'k_shot': 5, # train on this many samples from test domain
+    'test_domains': ['MR']
 }
 
 config = get_config(base_config, new_config)
--- configs/multitask_config.py
+++ configs/multitask_config.py
@@ -4,10 +4,16 @@ import logging
 from .base_config import base_config, get_config
 
 new_config = {
-    'exp_name': 'multitask_6-12',
+    'exp_name': 'multitask_6-11-3500-sports-dvd',
     'epochs': 1,
-    'unfreeze_layers': (7,8,9,10,11), # layers of bert to unfreeze
+    'unfreeze_layers': (6,7,8,9,10,11), # layers of bert to unfreeze
     'batch_size': 16,
+    'num_examples': 3500,
+    # 'train_domains':  ['apparel', 'baby', 'books', 'camera_photo', 'electronics',
+    #             'health_personal_care', 'kitchen_housewares', 'magazines',
+    #             'music', 'software', 'sports_outdoors', 'toys_games', 'video', 'dvd'],
+    # 'val_domains': ['imdb'],
+    # 'test_domains': ['MR'],
     'device': 'cuda'
 }
 
